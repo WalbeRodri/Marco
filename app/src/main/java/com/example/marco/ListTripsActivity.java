81d61afa6eea9bd2cc2dec354310fc241b74d0c1
Thiago Paim <thpaim@gmail.com>
2016-10-22 13:41:28 -0300

    Activity inicial e listagem de viagens com conteúdo estatico

---
+++ app/src/main/java/com/example/marco/ListTripsActivity.java
@@ -0,0 +1,13 @@
+package com.example.marco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListTripsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_trips);
+    }
+}
