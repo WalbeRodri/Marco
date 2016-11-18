42515035373dc76f3f109220503bfd276e57f777
Milena Carneiro <mscc@cin.ufpe.br>
2016-11-18 15:48:55 -0300

    Integração com Matheus

---
+++ app/src/main/java/database/dataBaseMarco.java
@@ -0,0 +1,69 @@
+package database;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import base.*;
+
+/**
+ * Created by Matheus on 06/11/2016.
+ */
+
+public class dataBaseMarco {
+    private FirebaseDatabase database;
+    private DatabaseReference tripRef;  //nó de viagem
+    private DatabaseReference perfilRef; // nó de perfil
+    private DatabaseReference localRef;
+
+    //  private DatabaseReference agendaRef;
+    public dataBaseMarco() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        String uidUser = auth.getCurrentUser().getUid();
+        database = FirebaseDatabase.getInstance();
+        localRef = database.getReference("local");
+        tripRef = database.getReference("trip").child(uidUser);
+        perfilRef = database.getReference("perfil").child(uidUser);
+    }
+
+
+    public void createPerfil(Perfil perfil) {
+        perfilRef.push().setValue(perfil);
+    }
+
+    public void createTrip(Trip trip) {
+       tripRef.push().setValue(trip);
+
+    }
+
+    public void editTrip(Trip trip,String key){
+        tripRef.child(key).setValue(trip);
+    }
+
+
+    public void editPerfil(Perfil perfil,String key){
+        perfilRef.child(key).setValue(perfil);
+    }
+
+    public void deleteTrip(String key) {
+        tripRef.child(key).removeValue();
+    }
+
+    public void deletePerfil(String key) {
+        perfilRef.child(key).removeValue();
+    }
+
+
+    public Query recoverPerfil() {;
+        return perfilRef;
+    }
+
+    public Query recoverTrips() {
+        return tripRef;
+    }
+
+    public Query recoverLocal() {
+       return  localRef;
+    }
+
+}
