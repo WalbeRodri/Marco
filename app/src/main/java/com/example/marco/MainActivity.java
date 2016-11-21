b736477cb922a2299034481eed7eae9ee906b647
jirumaru <nunozex987@hotmail.com>
2016-11-21 17:56:31 -0300

    Pegando dados do servidor no PerfilActivity

--- app/src/main/java/com/example/marco/MainActivity.java
+++ app/src/main/java/com/example/marco/MainActivity.java
@@ -17,9 +17,13 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    final DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/marco/PerfilActivity.java
+++ app/src/main/java/com/example/marco/PerfilActivity.java
@@ -10,7 +10,12 @@ import android.widget.CompoundButton;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 import java.util.*;
@@ -29,17 +34,26 @@ public class PerfilActivity extends AppCompatActivity {
     private Query mQuery; //caminho de Local
     private PerfilAdapter perfilAdapter; //adapter de perfil , não usado
 
+    final DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+
+    ToggleButton tbBar;
+    ToggleButton tbMuseu;
+    ToggleButton tbPraca;
+    ToggleButton tbPraia;
+    ToggleButton tbTeatro;
+    ToggleButton tbComida;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
 
-        ToggleButton tbBar = (ToggleButton) findViewById(R.id.tbBar);
-        ToggleButton tbMuseu = (ToggleButton) findViewById(R.id.tbMuseu);
-        ToggleButton tbPraca = (ToggleButton) findViewById(R.id.tbPraca);
-        ToggleButton tbPraia = (ToggleButton) findViewById(R.id.tbPraia);
-        ToggleButton tbTeatro = (ToggleButton) findViewById(R.id.tbTeatro);
-        ToggleButton tbComida = (ToggleButton) findViewById(R.id.tbComida);
+        tbBar = (ToggleButton) findViewById(R.id.tbBar);
+        tbMuseu = (ToggleButton) findViewById(R.id.tbMuseu);
+        tbPraca = (ToggleButton) findViewById(R.id.tbPraca);
+        tbPraia = (ToggleButton) findViewById(R.id.tbPraia);
+        tbTeatro = (ToggleButton) findViewById(R.id.tbTeatro);
+        tbComida = (ToggleButton) findViewById(R.id.tbComida);
 
         dbMarco = new dataBaseMarco(); //inicializando banco de dados
         setUpFirebase();
@@ -116,6 +130,44 @@ public class PerfilActivity extends AppCompatActivity {
                 }
             }
         });
+
+        //final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //DatabaseReference ref = database.getReference("https://marco-52cd1.firebaseio.com/");
+
+        // Attach a listener to read the data at our posts reference
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Perfil perfil1 = perfilAdapter.getItems().get(0);
+                Preferences pref = perfil1.getPreferences();
+                Toast.makeText(PerfilActivity.this, "Foi", Toast.LENGTH_SHORT).show();
+                if (pref.getPreferences().contains("Teatro")) {
+                    tbTeatro.setChecked(true);
+                }
+                if (pref.getPreferences().contains("Praia")) {
+                    tbPraia.setChecked(true);
+                }
+                if (pref.getPreferences().contains("Praca")) {
+                    tbPraca.setChecked(true);
+                }
+                if (pref.getPreferences().contains("Bar")) {
+                    tbBar.setChecked(true);
+                }
+                if (pref.getPreferences().contains("Comida")) {
+                    tbComida.setChecked(true);
+                }
+                if (pref.getPreferences().contains("Museu")) {
+                    tbMuseu.setChecked(true);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(PerfilActivity.this, "NAUM Foi", Toast.LENGTH_SHORT).show();
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+        //;
     }
 
     public void ConfirmaGosto(View view) {
