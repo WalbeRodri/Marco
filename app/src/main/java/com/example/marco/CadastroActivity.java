9c9f592cb26bffbe7c5db6c8b04a76508334382c
amnv <amnv@cin.ufpe.br>
2016-11-22 16:01:57 -0300

    new Branch

--- app/src/main/java/com/example/marco/CadastroActivity.java
+++ app/src/main/java/com/example/marco/CadastroActivity.java
@@ -1,9 +1,8 @@
 package com.example.marco;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,16 +15,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.api.model.StringList;
 
-import java.util.Calendar;
 import java.util.Date;
 
 import base.Perfil;
 import database.dataBaseMarco;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class CadastroActivity extends AppCompatActivity {
     protected Perfil perfil;
@@ -120,5 +114,7 @@ public class CadastroActivity extends AppCompatActivity {
             mAuth.removeAuthStateListener(mAuthListener);
         }
     }
+
+    public void cheat(){}
 }
 
